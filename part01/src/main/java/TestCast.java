2eb5418b37b88f4c1e4b52004fd626c1294c7099
pvercesi <vercesi@esteco.com>
2024-09-27 13:52:04 +0200

    small updates to part01

--- part01/src/main/java/TestCast.java
+++ part01/src/main/java/TestCast.java
@@ -5,10 +5,7 @@ public class TestCast {
         float c = 1002;      // 1002 is an int
         double d = 3.45F;    // 3.45F is a float
 
-        long e = 34;
-        int f = (int) e;          // e is a long
-        double g = 3.45;
-        float h = (float) g;      // g is a double
-
+        int f = (int) b;          // e is a long
+        float h = (float) d;      // g is a double
     }
 }
