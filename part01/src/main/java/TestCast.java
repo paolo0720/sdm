994ec5634ec55c9b8aee180d19eb9d9404d56c3f
pvercesi <vercesi@esteco.com>
2025-07-25 17:28:01 +0200

    type casting

--- part01/src/main/java/TestCast.java
+++ part01/src/main/java/TestCast.java
@@ -1,11 +1,16 @@
 public class TestCast {
     public static void main(String[] args) {
-        int a = 'x';        // 'x' is a character
+        int a = 'x';         // 'x' is a character
         long b = 34;         // 34 is an int
         float c = 1002;      // 1002 is an int
         double d = 3.45F;    // 3.45F is a float
 
-        int f = (int) b;          // e is a long
-        float h = (float) d;      // g is a double
+        int f = (int) b;     // e is a long
+        float h = (float) d; // g is a double
+        byte g = (byte) c;   // c is a float
+
+        System.out.println(f);
+        System.out.println(h);
+        System.out.println(g);
     }
 }
