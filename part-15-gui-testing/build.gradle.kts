0999bba5c0744e390b7250867d0ba80024f0e52d
pvercesi <vercesi@esteco.com>
2025-09-09 12:28:22 +0200

    cleaning

--- part-15-gui-testing/build.gradle.kts
+++ part-15-gui-testing/build.gradle.kts
@@ -1,21 +1,3 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
-}
-
-group = "it.esteco"
-version = "unspecified"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    id("common-build")
 }
