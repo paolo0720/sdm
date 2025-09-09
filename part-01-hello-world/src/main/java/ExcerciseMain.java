bfbe2ac6fc11d477ffc09de87b9f476f4a9cff57
pvercesi <vercesi@esteco.com>
2025-09-09 16:55:56 +0200

    part 01 - hello world - switch to Java 25

---
+++ part-01-hello-world/src/main/java/ExcerciseMain.java
@@ -0,0 +1,10 @@
+void main() {
+    IO.println("Hello, World!");
+}
+
+public class ExcerciseMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
